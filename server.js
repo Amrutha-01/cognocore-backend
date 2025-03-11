a851ef74bbd27a68b08dd48412965c9b14bd6208
Rokkam Lakshmi Amrutha <amrutharokkam573@gmail.com>
2025-03-11 20:33:20 +0530

    add cors url

--- server.js
+++ server.js
@@ -4,25 +4,21 @@ const connectDB=require('./db');
 const app=express();
 const cors=require('cors');
 app.use(
-  cors(
-    {
-    origin: "*",
+  cors({
+    origin: "https://cognocore-assignment.vercel.app/",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "id"],
-  }
-)
+  })
 );
 
 const userAPI=require('./routes/user-routes');
 const taskAPI=require('./routes/task-routes');
-const statsAPI=require('./routes/stats-routes');
 
 const port=5000;
 
 app.use(express.json());
 app.use("/api/auth/",userAPI);
 app.use("/api/tasks/",taskAPI);
-app.use('/api/stats/',statsAPI);
 
 connectDB();
 
